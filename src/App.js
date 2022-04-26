b5ffae1e20cd9bd6d3279ceda50a7d75a9743f19
maxisorichetti <maxisorichetti19@gmail.com>
2022-04-26 18:23:45 -0300

    asdfads

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import './App.css';
 
 function App() { return (
     <div className="App">
-    <h2>CHAUUUUUUU</h2>
-      <select>
-        ppapapa
-      </select>
-      <p>poooppopo</p>
-      <div>PEPEPEPEP </div>
-      <h1>PEPEP PIPIP POPO PPO  AOPAPUAUA </h1>
-      <h1>PEPEP PIPIP POPO PPO  AOPAPUAUA </h1>
+      <div className="table_btn">
+        <div>
+          <table className="table_success">
+            <tr>
+              <td>Producto</td>
+              <td>Nombre</td>
+              <td>Cantidad</td>
+              <td>Precio</td>
+            </tr>
+          </table>
+        </div>
+        </div>
     </div>
   );
 }
