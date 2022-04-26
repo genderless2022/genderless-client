2fb42c82cc2266fbb5232ebc0a907c8eb963b35e
maxisorichetti <maxisorichetti19@gmail.com>
2022-04-26 18:28:47 -0300

    wsdfasdf

--- src/App.js
+++ src/App.js
@@ -7,6 +7,21 @@ import './App.css';
 function App() { return (
     <div className="App">
       <p>Holaaaaaa</p>
+      <div className="table_btn">
+        <div>
+          <table className="table_success">
+            <tr>
+              <td>Producto</td>
+              <td>Nombre</td>
+              <td>Cantidad</td>
+              <td>Precio</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      <p>Otra prueba</p>
+      <p>Otra prueba</p>
+      <p>Otra prueba</p>
       <p>Otra prueba</p>
     </div>
   );
