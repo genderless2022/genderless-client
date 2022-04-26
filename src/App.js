231f666860cb529d2f9438ced29c66af562c744b
maxisorichetti <maxisorichetti19@gmail.com>
2022-04-26 18:17:22 -0300

    asdfasdf

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 
 function App() { return (
     <div className="App">
-      <p>Holaaaaaasadfasd</p>
+      <p>CHAUUUUUUU</p>
       <select>
         ppapapa
       </select>
       <p>peepep</p>
       <p>pipipip</p>
       <p>poooppopo</p>
-      <div>PEPEP PIPIP POPO PPO  AOPAPUAUA </div>
+      <h2>PEPEPEPEP </h2>
       <h1>PEPEP PIPIP POPO PPO  AOPAPUAUA </h1>
       <h1>PEPEP PIPIP POPO PPO  AOPAPUAUA </h1>
     </div>
