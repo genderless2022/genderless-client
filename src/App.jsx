85b3351195d99f20cad05729d81116e5a991f14a
TomasLaus <tomaslaus99@gmail.com>
2022-04-27 12:45:57 -0300

    Eslint y archivos jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Landing from './components/Landing/Landing';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+
+        <Route path="/" element={<Landing />} />
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
