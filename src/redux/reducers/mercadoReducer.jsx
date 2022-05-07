8482b22c6ff5237e54603c59813b9ea443ab259c
Mettfan <cesaryannick6@gmail.com>
2022-05-07 13:18:15 -0500

    Least MP actions added

--- src/redux/reducers/mercadoReducer.jsx
+++ src/redux/reducers/mercadoReducer.jsx
@@ -1,4 +1,4 @@
-import { MERCADO_CHECKOUT, ERROR } from '../actions/mercadopagoActions';
+import { MERCADO_CHECKOUT, ERROR, CHANGE_ORDER_STATE, GET_PAYMENTS, GET_USER_PAYMENTS, GET_AMOUNT_SOLD, GET_QUANTITY_SOLD } from '../actions/mercadopagoActions';
   
   const initialState = {
     status: '',
@@ -6,6 +6,16 @@ import { MERCADO_CHECKOUT, ERROR } from '../actions/mercadopagoActions';
   function mercadoReducer( state = initialState, action) {
     switch (action.type) {
       case MERCADO_CHECKOUT:
+        return { ...state, status: action.payload };
+      case CHANGE_ORDER_STATE:
+        return { ...state, status: action.payload };
+      case GET_PAYMENTS:
+        return { ...state, status: action.payload };
+      case GET_USER_PAYMENTS:
+        return { ...state, status: action.payload };
+      case GET_QUANTITY_SOLD:
+        return { ...state, status: action.payload };
+      case GET_AMOUNT_SOLD:
         return { ...state, status: action.payload };
 
       case ERROR:
