d367af180806c9c74325679ed4be66f7d326b806
Mettfan <cesaryannick6@gmail.com>
2022-05-10 11:17:44 -0500

    Documenting

--- src/components/ConnectGoogle/ConnectGoogle.jsx
+++ src/components/ConnectGoogle/ConnectGoogle.jsx
@@ -5,22 +5,34 @@ import { useNavigate } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 import { createUser } from '../../redux/actions/userActions';
 
+// Mi ID de cliente temporal (Requiere seguridad)
 const clientId = "141090610633-2n16fmmu0hek8ioah1m1o6508blcmn4t.apps.googleusercontent.com";
 
 function ConnectGoogle(props) {
+
+    //Hooks necesarios
     let nav = useNavigate()
     let cookie = new Cookies()
     let dispatch = useDispatch()
+
+    // Declaramos estados
     const [showloginButton, setShowloginButton] = useState(true);
     const [showlogoutButton, setShowlogoutButton] = useState(false);
    
-
+    //Lo que pasa cuando es autenticado correctamente por Google
     const onLoginSuccess = (res) => {
+
+        //User
         let user = res.profileObj
+
+        // clg
         console.log('Login Success:', user);
+
+        // Seteamos las cookies necesarias 9+
         cookie.set('googleUser', user)
-        cookie.set('user', { user: {
+        cookie.set('user', { user:  {
             
+            // user cookie format
             name: user.givenName,
             lastName: user.familyName,
             email: user.email,
@@ -41,12 +53,17 @@ function ConnectGoogle(props) {
         console.log('Login Failed:', res);
     };
 
+    // Lo que pasa cuando el usuario de desloguea
     const onSignoutSuccess = () => {
         alert("You have been logged out successfully");
         console.clear();
         setShowloginButton(true);
         setShowlogoutButton(false);
-        cookie.set('user', '')
+
+        // cookie erase
+        cookie.remove('user')
+
+        // cookie redirect to home
         !cookie.get('user') && props.redirectLogout && nav('/home')
         // setgoogleUser()
     };
@@ -55,6 +72,8 @@ function ConnectGoogle(props) {
         <div>
             {/* {googleUser?.givenName} */}
             { showloginButton && props.login ?
+
+                // Componente que google nos ofrece
                 <GoogleLogin
                     clientId={clientId}
                     buttonText="Conectate con Google"
@@ -65,6 +84,7 @@ function ConnectGoogle(props) {
                 /> : null}
 
             { showlogoutButton && props.logout ?
+// Componente que google nos ofrece
                 <GoogleLogout
                     clientId={clientId}
                     buttonText="Cerrar Sesión"
