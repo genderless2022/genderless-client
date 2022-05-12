de09724217ff9c945f771d19d4e71a229d41a0f5
Mettfan <cesaryannick6@gmail.com>
2022-05-12 04:08:21 -0500

    AddingChatBot

--- src/components/Chatbot/Chatbot.jsx
+++ src/components/Chatbot/Chatbot.jsx
@@ -3,16 +3,18 @@ import Chatbot from "react-chatbot-kit";
 import ActionProvider from "./ActionProvider";
 import config from "./config";
 import MessageParser from "./messageParser";
-
+import './Chatbot.css'
 export default function ChatBot (){
     return (<>
-
+        <div className="chatContainer">
         <Chatbot
             config={config}
             messageParser={MessageParser}
             actionProvider={ActionProvider}
         
         ></Chatbot>
+
+        </div>
     
     </>)
 }
